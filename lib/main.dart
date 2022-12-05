45c1f3f6c7943054669fc1b8299ada9d3e1f6bb7
ali <ali@Youths-iMac-6.local>
2022-12-05 12:23:31 +0400

    added local notification with firebase messaging

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,23 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:notifications/app.dart';
+import 'package:notifications/notifications/local_notification/LocalNotification.dart';
 
-import 'cloud_messaging/cloudMessaging.dart';
+import 'notifications/cloud_messaging/CloudMessaging.dart';
 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  final token = await FirebaseMessaging.instance.getToken();
 
-  print("token is : $token");
-  cloudMessaging.initialize();
+  CloudMessaging.initialize();
+  LocalNotification.initialize();
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   runApp(const MyApp());
 }
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  // await setupFlutterNotifications();
-  // showFlutterNotification(message);
-  // If you're going to use other Firebase services in the background, such as Firestore,
-  // make sure you call `initializeApp` before using other Firebase services.
-  print('Handling a background message ${message.messageId}');
+  debugPrint('Handling a background message ${message.notification?.title}');
 }
 
